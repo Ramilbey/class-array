954157897c247d3f76a41b859c395cccaeffa02d
Ramilbey <ramilbey505711503@gmail.com>
2025-04-13 23:55:49 +0800

    class polymorphism

--- array in all ways.cpp
+++ array in all ways.cpp
@@ -3,12 +3,28 @@ using namespace std;
 
 class Animal{
     public:
-        void voice() {
+        virtual void voice() {
             cout << "The animal sound" << endl;
         }
 };
-
+class Sheep : public Animal {
+    public: 
+    void voice() override{
+        cout << "Baa Baa Baa /n";
+    }
+};
+class Cat : public Animal {
+    public :
+    void voice () override{
+        cout << "Mewo Mewo Mewo /n";
+    }
+};
 
 int main (){
-    
+    Animal* a1 = new Sheep;
+    Animal* a2 = new Cat;
+    a1->voice();
+    a2->voice();
+    cout << a1 << " "<< a2;
+    return 0;
 }
