9115ec3d10759a38f5c8c62510ec9d74223afb76
Ramilbey <ramilbey505711503@gmail.com>
2025-04-13 23:39:16 +0800

    class and arrays

---
+++ array in all ways.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+using namespace std;
+
+class Animal{
+    public:
+        void voice() {
+            cout << "The animal sound" << endl;
+        }
+};
+
+
+int main (){
+    
+}
